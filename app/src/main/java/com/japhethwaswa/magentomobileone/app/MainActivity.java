9234f682d520dac3e2063c2e4ce6f5d458ff7f69
jefflilcot <japhethwaswa@gmail.com>
2017-02-07 10:29:12 +0300

    Splash screen created successfuly though consuming 7mb of memory

--- app/src/main/java/com/japhethwaswa/magentomobileone/app/MainActivity.java
+++ app/src/main/java/com/japhethwaswa/magentomobileone/app/MainActivity.java
@@ -1,7 +1,9 @@
-package com.japhethwaswa.magentomobileone;
+package com.japhethwaswa.magentomobileone.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.japhethwaswa.magentomobileone.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/japhethwaswa/magentomobileone/app/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.japhethwaswa.magentomobileone.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.japhethwaswa.magentomobileone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash);
+        /**Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();**/
+    }
+}
