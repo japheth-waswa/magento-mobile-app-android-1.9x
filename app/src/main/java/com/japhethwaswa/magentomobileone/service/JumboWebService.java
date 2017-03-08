3a381d9f297f6fcf8bc1e937b463e3f29bd95f6e
jeff lilcot <jeff lilcot>
2017-03-08 12:02:51 +0000

    succesful insert through jobManager

--- app/src/main/java/com/japhethwaswa/magentomobileone/service/JumboWebService.java
+++ app/src/main/java/com/japhethwaswa/magentomobileone/service/JumboWebService.java
@@ -1,7 +1,10 @@
 package com.japhethwaswa.magentomobileone.service;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 
 import com.androidnetworking.AndroidNetworking;
@@ -11,6 +14,8 @@ import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.StringRequestListener;
 import com.japhethwaswa.magentomobileone.R;
+import com.japhethwaswa.magentomobileone.db.JumboContract;
+import com.japhethwaswa.magentomobileone.db.JumboQueryHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,22 +29,56 @@ import java.net.URL;
 public class JumboWebService {
 
 
-    public static void retrieveMainData(Context context){
+    public static void retrieveMainData(final Context context) {
         //get relative url for this service
         Resources res = context.getResources();
         String relativeUrl = res.getString(R.string.jumbo_main_data_url);
 
-        AndroidNetworking.get(getAbsoluteUrl(context,relativeUrl))
+        AndroidNetworking.get(getAbsoluteUrl(context, relativeUrl))
                 .setTag("MainDataRequest")
-        .setPriority(Priority.HIGH)
-        .build().getAsJSONObject(new JSONObjectRequestListener() {
+                .setPriority(Priority.HIGH)
+                .build().getAsJSONObject(new JSONObjectRequestListener() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.e("MainDataRequest",response.toString());
+                Log.e("MainDataRequest", response.toString());
                 //parse the json object extracting pager data and main data
                 try {
                     JSONArray pagerArray = response.getJSONArray("pager");
-                    Log.e("MainDataPagerArray",pagerArray.toString());
+                    // pagerArray.length();
+                    if (pagerArray.length() > 0) {
+                        updatePager(pagerArray, context);
+                    }
+                    Log.e("MainDataPagerArray", pagerArray.toString());
+                    Log.e("MainDataPagerArrLength", String.valueOf(pagerArray.length()));
+                    
+                    /**==**/
+                    String[] projection = {
+                            JumboContract.PagerEntry.COLUMN_TITLE,
+                            JumboContract.PagerEntry.COLUMN_BRIEF_DESCRIPTION,
+                            JumboContract.PagerEntry.COLUMN_IMAGE_URL_LOCAL,
+                            JumboContract.PagerEntry.COLUMN_IMAGE_URL_REMOTE,
+                            JumboContract.PagerEntry.COLUMN_UPDATED_AT
+                    };
+                    JumboQueryHandler handler = new JumboQueryHandler(context.getContentResolver()){
+                        @Override
+                        protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+                            try{
+                                if(cursor != null){
+                                    while(cursor.moveToNext()){
+                                        String title = cursor.getString(cursor.getColumnIndex(JumboContract.PagerEntry.COLUMN_TITLE));
+                                        String updated_at = cursor.getString(cursor.getColumnIndex(JumboContract.PagerEntry.COLUMN_UPDATED_AT));
+                                        Log.e("item-val",title);
+                                        Log.e("item-val",updated_at);
+                                    }
+                                }
+                            }finally{
+
+                            }
+                        }
+                    };
+                    handler.startQuery(1, null, JumboContract.PagerEntry.CONTENT_URI, projection, null, null, null);
+                    /**===**/
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -47,7 +86,7 @@ public class JumboWebService {
 
             @Override
             public void onError(ANError anError) {
-                Log.e("MainDataRequestError",anError.toString());
+                Log.e("MainDataRequestError", anError.toString());
             }
         });
 
@@ -55,38 +94,67 @@ public class JumboWebService {
         /**try{
 
 
-            URL url = new URL(getAbsoluteUrl(context,relativeUrl));
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-            try{
-                //process the request.
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while((line = bufferedReader.readLine()) != null){
-                    stringBuilder.append(line).append("\n");
-                }
-                //close the bufferedReader
-                bufferedReader.close();
-                Log.e("Response",stringBuilder.toString());
-                return stringBuilder.toString();
+         URL url = new URL(getAbsoluteUrl(context,relativeUrl));
+         HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+         try{
+         //process the request.
+         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+         StringBuilder stringBuilder = new StringBuilder();
+         String line;
+         while((line = bufferedReader.readLine()) != null){
+         stringBuilder.append(line).append("\n");
+         }
+         //close the bufferedReader
+         bufferedReader.close();
+         Log.e("Response",stringBuilder.toString());
+         return stringBuilder.toString();
 
-            }finally {
+         }finally {
 
-                //disconnect
-                urlConnection.disconnect();
+         //disconnect
+         urlConnection.disconnect();
+         }
+
+         }catch (Exception e){
+         Log.e("ERROR-CONN",e.getMessage(),e);
+
+         }
+         return null;**/
+    }
+
+    private static void updatePager(JSONArray pagerArray, Context context) {
+
+        JumboQueryHandler handler = new JumboQueryHandler(context.getContentResolver());
+        //delete all previous data
+        handler.startDelete(2,null,JumboContract.PagerEntry.CONTENT_URI,null,null);
+
+        //perform new inserts
+        for (int i = 0; i <= pagerArray.length(); i++) {
+            try {
+
+                JSONObject pagerObject = pagerArray.getJSONObject(i);
+                Log.e("item-1", pagerObject.getString("title"));
+
+                ContentValues values = new ContentValues();
+                values.put(JumboContract.PagerEntry.COLUMN_TITLE, pagerObject.getString("title"));
+                values.put(JumboContract.PagerEntry.COLUMN_BRIEF_DESCRIPTION, pagerObject.getString("brief_description"));
+                values.put(JumboContract.PagerEntry.COLUMN_IMAGE_URL_LOCAL, pagerObject.getString("image_url_local"));
+                values.put(JumboContract.PagerEntry.COLUMN_IMAGE_URL_REMOTE, pagerObject.getString("image_url_remote"));
+                values.put(JumboContract.PagerEntry.COLUMN_UPDATED_AT, pagerObject.getString("updated_at"));
+
+                //perform new insert
+                handler.startInsert(1, null, JumboContract.PagerEntry.CONTENT_URI, values);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-
-        }catch (Exception e){
-            Log.e("ERROR-CONN",e.getMessage(),e);
-
         }
-        return null;**/
     }
 
 
-    private static String getAbsoluteUrl(Context context,String relativeUrl){
+    private static String getAbsoluteUrl(Context context, String relativeUrl) {
         Resources res = context.getResources();
         String baseUrl = res.getString(R.string.apiBaseUrl);
-        return  baseUrl + relativeUrl;
+        return baseUrl + relativeUrl;
     }
 }
