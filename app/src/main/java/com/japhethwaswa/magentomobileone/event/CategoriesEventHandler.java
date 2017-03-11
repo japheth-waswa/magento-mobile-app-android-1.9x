5e31f34732b0b7fb06c3e3225f5c0613bae5c1ba
jeff lilcot <jeff lilcot>
2017-03-11 15:39:24 +0000

    home fragment completed

---
+++ app/src/main/java/com/japhethwaswa/magentomobileone/event/CategoriesEventHandler.java
@@ -0,0 +1,12 @@
+package com.japhethwaswa.magentomobileone.event;
+
+import android.util.Log;
+import android.view.View;
+
+public class CategoriesEventHandler {
+
+    public void onImageClicked(View view,String tag){
+        Log.e("jeff","image item clicked");
+    }
+
+}
